8c3c7fc944acc872e1ccae4d09d9bc5f88159b60
Ogeidy <yakov-mamontov@yandex.ru>
2015-09-23 22:26:47 +0300

    first commit

---
+++ src/vk_first.java
@@ -0,0 +1,26 @@
+import java.net.Socket;
+
+public class vk_first {
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		
+		try {
+			Socket sock = new Socket("api.vk.com", 80);
+			
+			String req = "GET http://api.vk.com/method/users.get.xml?user_ids=1&fields=bdate HTTP/1.0 \n\n";
+			
+			sock.getOutputStream().write(req.getBytes());
+			
+			byte buf[] = new byte[64*1024];
+			int r = sock.getInputStream().read(buf);
+			String data = new String(buf, 0, r);
+			
+			System.out.println(data);
+			
+			sock.close();
+		}
+		catch(Exception e) {
+			System.out.println("Error: "+e);
+		}
+	}
+}

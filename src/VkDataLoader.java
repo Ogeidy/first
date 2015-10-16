174738a0ba32a35c26dd157a791af752de4fe5b0
Ogeidy_net <yakov-mamontov@yandex.ru>
2015-10-16 13:14:16 +0300

    Added the check VK timeout

--- src/VkDataLoader.java
+++ src/VkDataLoader.java
@@ -23,12 +23,17 @@ public class VkDataLoader {
 //		prnt.printResult(result);	
 		
 		for (int i = 0; i < conf.universityNum; i++) {
+			
 			int Uni = Integer.parseInt(conf.universities[i].get("id").toString());
 			int numFcts = Integer.parseInt(((JSONObject)conf.faculties[i].get("response")).get("count").toString());
+			
 			for (int j = 0; j < numFcts; j++){
+				
 				JSONArray arrFcts = (JSONArray)((JSONObject)conf.faculties[i].get("response")).get("items");
 				int idFct = Integer.parseInt(((JSONObject)arrFcts.get(j)).get("id").toString());
-				for (int k = conf.yearFrom; k <= conf.yearTo; k++) {         
+				
+				for (int k = conf.yearFrom; k <= conf.yearTo; k++) {      
+					
 					System.out.println("i:"+i+" j:"+j+" k:"+k+" ID Fct:"+idFct);
 					
 					long start = System.currentTimeMillis();
@@ -45,7 +50,16 @@ public class VkDataLoader {
 					
 					long finish = System.currentTimeMillis();
 					int tm = (int)(finish-start);
-					System.out.println("Time:"+tm+"ms");	
+					System.out.println("Time:"+tm+"ms");
+					
+					// Check the VK timeouts
+					if (tm < 334) {
+						try {
+							Thread.sleep(334 - tm);
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+					}
 				}
 			}
 		}

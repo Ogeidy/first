2a38c2d8f2a314bfc9b71358cd35673af8ebd6c9
Ogeidy_net <yakov-mamontov@yandex.ru>
2015-10-31 14:06:58 +0300

    Test sommit

--- src/main/java/com/vkbigdata/vkdataloader/VkDataLoader.java
+++ src/main/java/com/vkbigdata/vkdataloader/VkDataLoader.java
@@ -4,7 +4,6 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-
 public class VkDataLoader {
 	
 	private static String CONFIG_FILE = "config_spbsu.txt";
